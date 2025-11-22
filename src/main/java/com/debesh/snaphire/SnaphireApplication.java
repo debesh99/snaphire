64bce27dc566e88c774240cff529f4fbed8d1e2c
dpadhy1 <debeshpadhy99@gmail.com>
2025-11-22 18:56:03 +0530

    Added sprinf folders

---
+++ src/main/java/com/debesh/snaphire/SnaphireApplication.java
@@ -0,0 +1,13 @@
+package com.debesh.snaphire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnaphireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnaphireApplication.class, args);
+	}
+
+}
